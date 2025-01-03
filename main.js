108c838fccf305fcb8eb4a73552cf468d2896203
Pluto <devvaannsh@gmail.com>
2025-01-03 20:25:18 +0530

    feat: add Settings button in file menu that opens/closes settings panel

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-/*global define, brackets, $ */
-
-// See detailed docs in https://docs.phcode.dev/api/creating-extensions
-
-// A good place to look for code examples for extensions: https://github.com/phcode-dev/phoenix/tree/main/src/extensions/default
-
-// A simple extension that adds an entry in "file menu> hello world"
 define(function (require, exports, module) {
     "use strict";
-
-    // Brackets modules
     const AppInit = brackets.getModule("utils/AppInit"),
-        DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
-        Dialogs = brackets.getModule("widgets/Dialogs"),
         CommandManager = brackets.getModule("command/CommandManager"),
-        Menus = brackets.getModule("command/Menus");
+        Menus = brackets.getModule("command/Menus"),
+        WorkspaceManager = brackets.getModule("view/WorkspaceManager");
 
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        Dialogs.showModalDialog(
-            DefaultDialogs.DIALOG_ID_INFO,
-            "hello",
-            "world"
-        );
+    let pluginPanel; // Store panel reference
+    
+    let panelMinSize = window.innerWidth / 3;
+    
+    function togglePanelVisibility() { 
+        if (pluginPanel.isVisible()) {
+            pluginPanel.hide();
+        } else {
+            pluginPanel.show();
+        }
     }
-    
-      // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    function handleSettingsButtonClick() {
+        if (!pluginPanel) {
+            const $panel = $("<div>")
+                .attr("id", "phoenix-settings-panel")
+                .html("<h3>My Plugin Panel</h3><p>Hello from the panel!</p>");
+            const $toolbarIcon = $("<a>");
+            pluginPanel = WorkspaceManager.createPluginPanel(
+                "phoenix.settings", $panel, panelMinSize, $toolbarIcon
+            );
+        }
+        togglePanelVisibility();
+    }
+
+    const MY_COMMAND_ID = "phoenix_settings";
+    CommandManager.register("Settings", MY_COMMAND_ID, handleSettingsButtonClick);
+    
+    const menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
     menu.addMenuItem(MY_COMMAND_ID);
-    
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-    
-    // Initialize extension once shell is finished initializing.
+
     AppInit.appReady(function () {
-        console.log("hello world");
+        console.log("Phoenix Settings Extension Initialized!");
     });
 });
